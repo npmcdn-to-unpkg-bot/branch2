ad5c693c8931ea6ef991ce1f6c7ee595216a1fff
inventoryUI <warren@dub.local>
2016-05-11 19:33:48 -0400

    changes

--- trainAngular/src/app.js
+++ trainAngular/src/app.js
@@ -2,23 +2,9 @@
 
 angular.module('myApp', []);
 
-angular.module('myApp').controller('MainController', ['$scope','$interval', function($scope,$interval){
+angular.module('myApp').controller('MainController', ['$scope', function($scope){
 	console.log('hello');
 	$scope.randomValue=-999;
-	$scope.number=1;
-	
-	$interval(function(){
-		$scope.randomValue=Math.round(Math.random() * 100);
-	},2000);
-	
-	
-	$scope.sayHowdy=function(name){
-		return $scope.message+ ' '+ name;
-	};
-	
-		$scope.addNum=function(){
-		$scope.message2=days[$scope.index];
-		return $scope.message2;
-	};
+
 
 }]);
