57cc6093cba7270670f69bf624420289b579b328
inventoryUI <warren@dub.local>
2016-05-16 15:21:15 -0400

    before

--- triAngular/src/app.js
+++ triAngular/src/app.js
@@ -5,7 +5,8 @@ angular.module('myApp', []);
 angular.module('myApp').controller('MainController', ['$scope', function($scope){
 	console.log('hello');
 	$scope.randomValue=-999;
-
+	
+	this.newlist=["sunday","monday","tuesday"];
 
 	this.user1={
 		name:'billy joe',
@@ -19,6 +20,6 @@ angular.module('myApp').filter('first', function(){
 	var result=null;
 	return function(value){
 	
-	result=value+"al";
+	result=value+"ar";
 		return result;};
 });
